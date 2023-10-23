62363a548858bdf9ec6cdb8a94b0488d58e3a7e7
smg78E4 <n.p.plpl>
2023-10-23 19:06:52 +0300

    samo levski

---
+++ db_scripts/create_script.sql
@@ -0,0 +1,8 @@
+create database company_ms;
+
+create table users (
+id int not null AUTO_INCREMENT PRIMARY key,
+    username varchar(20) not null,
+    pass varchar(20) not null,
+    created timestamp
+);
