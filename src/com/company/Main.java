a66d67eeaeb357c244c6fdc10d4e6b41e131fa12
smg78E4 <n.p.plpl>
2023-10-23 19:08:15 +0300

    samo levski

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-System.out.println("GOLQQQQMA PROMQNA");
+
     public static void main(String[] args) throws SQLException {
         List<String> users = DBManager.getInstance().getAllUsers();
         for (String username : users) {
